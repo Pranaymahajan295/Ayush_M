ecdfe8a2ac5c8a3caa465295486256581af10e51
Pranay Mahajan <pranaymahajan006@gmail.com>
2025-04-06 01:04:28 -0700

    Add files via upload

---
+++ Js/index.js
@@ -0,0 +1,23 @@
+// console.log("hello World");
+// alert("What is your Name");
+// window.prompt("What is calculation ")
+// addition 
+// let a = 10;
+// let b = 50;
+// z = a + b 
+// console.log(z)
+
+// // substraction
+// let x = 10;
+// let y = 50;
+// z = x - y ; 
+// console.log(z)
+
+// // multiplication 
+// let  = 10;
+// let  = 50;
+// z = x - y ; 
+// console.log(z)
+
+
+
